f6618fc65c28e97e814085f1544a2427ee2aca2c
Tokuchi Yugo <yugo_tokuchi@metaps-pn150.local>
2019-06-28 23:43:20 +0900

    change for fourth episode

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
       <app-header></app-header>
-      <app-menu></app-menu>
-      Wallet:<input v-model="walletAddress"  size="50"></input>
-      <button v-on:click="getJsonFromGivenWallet">get</button><br>
-      My Wallet:{{this.msg}}<button v-on:click="getJsonFromMyWallet">get my assets</button>
+      <app-menu></app-menu>      Wallet:<input v-model="this.walletAddress"  size="50"></input>
+      <button v-on:click="this.items = getHisAssets">get</button>
+      <button v-on:click="this.items = getMyAssets">get my assets</button>
 
       <app-item-grid :items="items"></app-item-grid>
   </div>
@@ -18,7 +17,7 @@ import Web3 from 'web3'
 import axios from 'axios'
 const tokenABI =[{"constant":true,"inputs":[{"name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokenId","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isPauser","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokenId","type":"uint256"},{"name":"tokenURI","type":"string"}],"name":"mintWithTokenURI","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renouncePauser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_extensionType","type":"uint16"},{"name":"_supplyLimit","type":"uint16"}],"name":"setSupplyLimit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addPauser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_extensionType","type":"uint16"}],"name":"getSupplyLimit","outputs":[{"name":"","type":"uint16"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tokenURIPrefix","type":"string"}],"name":"setTokenURIPrefix","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"isAlreadyMinted","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokenId","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"tokenURIPrefix","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"EXTENSION_TYPE_OFFSET","outputs":[{"name":"","type":"uint16"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_owner","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"mintExtensionAsset","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpaused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"PauserAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"PauserRemoved","type":"event"},{"anonymous":false,"inputs":[],"name":"MintingFinished","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterRemoved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":true,"name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"approved","type":"address"},{"indexed":true,"name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"operator","type":"address"},{"indexed":false,"name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"}]
 const contractHashMCHE = "0xdceaf1652a131F32a821468Dc03A92df0edd86Ea"
-let web3,myAddress
+let web3
 async function startup() {
     console.log('starapp')
     if (window.ethereum) {
@@ -40,8 +39,11 @@ async function startup() {
     else {
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
+
 }
 startup()
+
+
 const contract = web3.eth.Contract(tokenABI, contractHashMCHE)
 function getJsonFromTokenIds(tokenids){
   let output = []
@@ -57,61 +59,53 @@ export default {
   name: 'app',
   data () {
     return {
-      items: [],
+      items: ["waiting for load items"],
       count: 0,
       walletAddress: "0x9c36edd80831f7e0d95131c076ef918b056967cf",
-      msg: ""
+      msg: "",
+      tokenindexes: []
     }
   },
   mounted: async function(){
     console.log("mounted")
-    await web3.eth.getAccounts().then(function(value){myAddress=value[0]})
-    this.msg = myAddress
-
-    console.log(this.items)
+    console.log("mounted:"+ this.getMyWallet)
     let tokenids2 = []
     for(let i=0;i<10;i++){
       await contract.methods.tokenByIndex(i).call().then(function(value){
         tokenids2.push(parseInt(value._hex,16))
       })
     }
+    this.tokenindexes=tokenids2
     this.items = getJsonFromTokenIds(tokenids2)
 
   },
-  methods: {
-    getJsonFromGivenWallet: async function () {
-      let num = 0
-      await contract.methods.balanceOf(this.walletAddress).call().then(function(value){
-        num = parseInt(value._hex,16)
-        console.log(num)
-      })
-
-      let tokenids = []
-      for(let i=0;i<num;i++){
-        await contract.methods.tokenOfOwnerByIndex(this.walletAddress,i).call().then(function(value){
-          tokenids.push(parseInt(value._hex,16))
-        })
-      }
-
-      this.items=getJsonFromTokenIds(tokenids)
-    },
-    getJsonFromMyWallet: async function () {
-      this.msg = myAddress
-      let num = 0
-      await contract.methods.balanceOf(this.msg).call().then(function(value){
-        num = parseInt(value._hex,16)
-      })
-      let tokenids = []
-      for(let i=0;i<num;i++){
-        await contract.methods.tokenOfOwnerByIndex(this.msg,i).call().then(function(value){
-          tokenids.push(parseInt(value._hex,16))
-        })
-      }
-
-      this.items=getJsonFromTokenIds(tokenids)
-
+  computed:{
+    getMyAssets: async function(){
+      let adr
+      await web3.eth.getAccounts().then(function(value){adr = value[0]})
+      return this.getJsonFromWalletAddress(adr)
+    } ,
+    getHisAssets: function(){
+      return this.getJsonFromWalletAddress(this.walletAddress)
     }
 
+  },
+  methods: {
+    getJsonFromWalletAddress: async function (address) {
+      let num = 0
+      await contract.methods.balanceOf(address).call().then(function(value){
+        num = parseInt(value._hex,16)
+      })
+
+      let tokenids = []
+      for(let i=0;i<num;i++){
+        await contract.methods.tokenOfOwnerByIndex(address,i).call().then(function(value){
+          tokenids.push(parseInt(value._hex,16))
+        })
+      }
+
+      this.items=getJsonFromTokenIds(tokenids)
+    }
   },
   components: {
     appHeader: Header,
